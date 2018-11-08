a448067c5004660a0eccf18b82213869cfaca7fb
mati23 <mateusarruda@alu.ufc.br>
2018-11-07 21:33:09 -0300

    Teste

--- users/urls.py
+++ users/urls.py
@@ -6,6 +6,7 @@ from . import views
 
 app_name = 'users'
 urlpatterns = [
-    #pagina inicial
-    path('login/', auth_views.LoginView.as_view(template_name = 'users/login.html'),name='login'),
+    #pagina do login
+    path('login/', auth_views.LoginView.as_view(template_name = 'users/login.html')
+    ,name='login'),
 ]
