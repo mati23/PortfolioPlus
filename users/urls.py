b3a10416636d0fe86ece2875a67a35f6e0e83c26
mati23 <mateusarruda@alu.ufc.br>
2018-11-11 14:58:15 -0300

    Logout

--- users/urls.py
+++ users/urls.py
@@ -9,4 +9,5 @@ urlpatterns = [
     #pagina do login
     path('login/', auth_views.LoginView.as_view(template_name = 'users/login.html')
     ,name='login'),
+    path('logout/', views.logout_view, name='logout'),
 ]
--- users/views.py
+++ users/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.urls import reverse
+from django.http import HttpResponseRedirect
+from django.contrib.auth import logout
+
 
 # Create your views here.
+
+def logout_view(request):
+    """Faz o logout do usuario"""
+    logout(request)
+    return HttpResponseRedirect(reverse('portfolio:index'))
