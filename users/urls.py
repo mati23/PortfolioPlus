93ad58dd496c9e60abdd54c603ab0d5dc906bdab
mati23 <mateusarruda@alu.ufc.br>
2018-11-11 16:22:10 -0300

    Logout

--- users/urls.py
+++ users/urls.py
@@ -9,5 +9,6 @@ urlpatterns = [
     #pagina do login
     path('login/', auth_views.LoginView.as_view(template_name = 'users/login.html')
     ,name='login'),
+    path('profile_page', views.profile_view, name ='profile_page'),
     path('logout/', views.logout_view, name='logout'),
 ]
--- users/views.py
+++ users/views.py
@@ -10,3 +10,6 @@ def logout_view(request):
     """Faz o logout do usuario"""
     logout(request)
     return HttpResponseRedirect(reverse('portfolio:index'))
+
+def profile_view(request):
+    return render(request, 'users/profile_page.html')
